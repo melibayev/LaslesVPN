e749c4da5a8edf4325fd76abbc032029de99c948
“melibayev” <melibayefff1@gmail.com>
2023-05-04 03:08:07 +0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,71 @@
+// HAMBURGER navbar-open-close
+function openNavbar() {
+    document.getElementById("navbar-responsive").style.right = "0";
+  }
+  function closeNavbar() {
+    document.getElementById("navbar-responsive").style.right = "-100%";
+  }
+  
+  document.getElementById("navbar-open").addEventListener("click", openNavbar);
+  document.getElementById("navbar-close").addEventListener("click", closeNavbar);
+
+
+// CAROUSEL 
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        800:{
+            items:2
+        },
+        1200:{
+            items:3
+        }
+    }
+})
+
+// BACKTOP function
+window.addEventListener("scroll", function () {
+    toggleBacktop();
+});
+
+let backtop = document.getElementById("backtop");
+
+function toggleBacktop() {
+    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+        backtop.style.bottom = "50px";
+    } else {
+        backtop.style.bottom = "-50px";
+    }
+}
+
+// Navbar Shrink
+window.addEventListener("scroll", function () {
+    shrink();
+});
+
+let navbar = document.getElementById("navbar");
+
+function shrink() {
+    if (scrollY > 100) {
+        navbar.classList.add("navbar-shrink");
+    } else {
+        navbar.classList.remove("navbar-shrink");
+    }
+}
+
+// DARK-LIGHT function
+let modeBtn = document.getElementById("dark-light");
+
+modeBtn.addEventListener("click", function () {
+  if (document.body.className != "dark") {
+    this.firstElementChild.src = "images/light.svg";
+  } else {
+    this.firstElementChild.src = "images/dark.svg";
+  }
+  document.body.classList.toggle("dark");
+});
